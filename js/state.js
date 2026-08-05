e51bd43e6e6151bfa6a3c7c8b154a0a3c81eebb7
Burzhuys-dice <zavinikita@gmail.com>
2026-08-05 10:11:48 +0300

    Create state.js

---
+++ js/state.js
@@ -0,0 +1,32 @@
+// Початковий стан персонажа
+export const characterState = {
+    concept: {
+        name: "",
+        clan: null,
+        predatorType: null
+    },
+    attributes: {
+        strength: 1, 
+        dexterity: 1, 
+        stamina: 1,
+        charisma: 1,
+        manipulation: 1,
+        composure: 1,
+        intelligence: 1,
+        wits: 1,
+        resolve: 1
+    },
+    skills: {},
+    disciplines: []
+};
+
+// Централізована функція оновлення атрибутів
+export function updateAttribute(attr, value) {
+    characterState.attributes[attr] = value;
+    
+    // Генеруємо подію, щоб UI знав, що дані змінилися і їх треба перемалювати
+    const event = new CustomEvent('stateChanged', { 
+        detail: { type: 'attribute', attr: attr, value: value } 
+    });
+    document.dispatchEvent(event);
+}
